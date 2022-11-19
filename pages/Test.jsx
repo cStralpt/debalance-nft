7f306899805aede4b509f07add9bc57f9a0cfaef
Satria Alipatullah <satrialipatullah@gmail.com>
2022-11-19 19:25:48 +0700

    add content and make layouting for Story Section

---
+++ pages/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Test = () => {
+  return <div className="h-screen border-4">
+    <div className="h-full border-4 overflow-hidden overflow-x-auto">
+        <div className="h-"></div>
+    </div>
+  </div>;
+};
+
+export default Test;
